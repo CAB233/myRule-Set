66ec19feaf36af33b8999de24be12f39a76f7824
CAB233 <yidaduizuoye@outlook.com>
2025-05-27 15:39:28 +0800

    Update CI

--- custom/patch.sh
+++ custom/patch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jq '(.rules[].domain_suffix | arrays) |= map(select(. != "com.cn"))' domestic.json | sponge domestic.json
+jq '(.rules[].domain_suffix | arrays) |= map(select(. != "com.cn"))' non_ip/domestic.json | sponge non_ip/domestic.json
